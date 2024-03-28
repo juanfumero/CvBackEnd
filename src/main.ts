a49141df19eef0e283cc6e5417d8c4be4518291f
Juan Fumero <ingjef@gmail.com>
2024-03-28 12:37:47 +0100

    Posible solution to CORS

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const cors = require('cors');
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes( new ValidationPipe)
+  app.use(cors());
   await app.listen(3000);
 }
 bootstrap();
